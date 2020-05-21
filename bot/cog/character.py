4413b5367dd0a15a344c33be5e3ec054f93df427
SamuelHillis <Samuel Thomas Porter Hillis>
2020-05-21 16:25:06 -0600

    Basic character commands

--- bot/cog/character.py
+++ bot/cog/character.py
@@ -21,17 +21,19 @@ class Characters(Cog):
             await ctx.send("Usage: character {add|set|remove|view} {info}")
             return
 
-        cmd = argc.remove(0)
+        cmd = argc[0]
+        argc = list(argc[1:])
         if (cmd == "add"):
             await self.add_character(ctx, argc)
-        #elif (cmd == "set"):
-        #    await self.set_character(ctx, argc)
+        elif (cmd == "set"):
+            await self.set_character(ctx, argc)
         elif (cmd == "remove"):
             await self.del_character(ctx, argc)
         elif (cmd == "view"):
             await self.view_characters(ctx, ctx.message.author)
         else:
-            await ctx.send("Add command options")
+            argc.insert(0, cmd)
+            await self.get_character(ctx, argc)
 
 
     async def add_character(self, ctx, argc):
@@ -41,31 +43,32 @@ class Characters(Cog):
 
         name = " ".join(argc)
         cur = conn.cursor()
-        cur.execute(f"""
-            INSERT INTO Character VALUES (PlayerID, Name)
-            (?, ?)
-        """, (ctx.message.author.id, name))
+        cur.execute("""
+            INSERT INTO Character (PlayerID, Name)
+            VALUES (?, ?)
+        """, (ctx.message.author.id, name,))
 
         await ctx.send(f"Added {name} to {ctx.message.author}'s characters!")
-
+        conn.commit()
+    
 
     async def del_character(self, ctx, argc):
         if (len(argc) < 2):
             await ctx.send("Usage: character remove [Character Name]")
             return
 
-        name = " ".join(argc[1:])
+        name = " ".join(argc)
         cur = conn.cursor()
-        cur.execute(f"""
+        cur.execute("""
             DELETE FROM Character
             WHERE PlayerID == ? AND Name == ?
-        """, (ctx.message.author.id, name))
+        """, (ctx.message.author.id, name,))
 
-        if (cur.fetchone() > 0):
+        if (cur.fetchone() != None):
             await ctx.send(f"{name} has been deleted!")
         else:
             await ctx.send(f"You don't have a character named {name}!")
-
+        conn.commit()
 
 
     async def view_characters(self, ctx, member: Member):
@@ -74,7 +77,7 @@ class Characters(Cog):
             return
 
         cur = conn.cursor()
-        cur.execute(f"""
+        cur.execute("""
             SELECT Name FROM Character
             WHERE PlayerID == ?
         """, (member.id,))
@@ -83,15 +86,34 @@ class Characters(Cog):
         out = ""
 
         if (len(chars) > 0):
-            out += f"{member}'s characters are:\n>>> "
-
-            for c in cur.fetchall():
-                out += c[0] + ":\n" + c[1] + "\n"
-            out = out[:-2]
+            out += f">>> {member}'s characters are:\n "
+            for c in chars:
+                out += c[0] + "\n"
+            out.strip()
         else:
             out += f"{member} has no characters!"
 
         await ctx.send(out)
+
+    
+    async def get_character(self, ctx, argc):
+        if (len(argc) == 0):
+            await ctx.send("Please give me a character name!")
+            return
+
+        name = " ".join(argc)
+        cur = conn.cursor()
+        cur.execute(f"""
+            SELECT Description FROM Character
+            WHERE PlayerID == ? AND Name == ?
+        """, (ctx.message.author.id, name))
+
+        character = cur.fetchone()
+        
+        if (character == None):
+            await ctx.send(f"No character named {name}!")
+        else:
+            await ctx.send(f">>> **{name}**\n{character[0]}")
 
 
 def setup(bot: Bot) -> None:
