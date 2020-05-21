cc30da971e45e65facf4e01749c8d582a76edc75
SamuelHillis <Samuel Thomas Porter Hillis>
2020-05-20 23:05:36 -0600

    Added token initialization

--- bot/__main__.py
+++ bot/__main__.py
@@ -8,13 +8,20 @@ import discord
 from bot import Bot
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = None
+
+try:
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+except:
+    env = open(".env", 'w+')
+    TOKEN = input("Enter Discord token: ")
+    env.write(f"DISCORD_TOKEN={TOKEN}")
 
 bot = Bot(command_prefix=".")
 
 bot.load_extension("cog.character")
 bot.load_extension("cog.roles")
-bot.load_extension("cog.rulebook")
+#bot.load_extension("cog.rulebook")
 
 bot.run(TOKEN)
